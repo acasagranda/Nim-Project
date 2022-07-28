6c508415845dc61b88bdde84dd7632cb9d768235
Andrea Casagranda <ybblkblt@yahoo.com>
2022-07-28 07:46:52 -0400

    Add determine player move

--- first_try.py
+++ first_try.py
@@ -1,9 +1,10 @@
 import random
+import time
 
 def intro_screen():
   print('                   PLAY NIM!!')
   print('*****************************************************')
-  print('Force the computer to choose the last star to win!!!')
+  print('         Remove the last star to win!!!')
   print('*****************************************************')
   ready_to_play = False
   while ready_to_play == False:
@@ -30,8 +31,30 @@ def print_screen(row_size):
   print('\nROW 3: ',end=' ')
   for i in range(row_size[3]):
     print('*', end=' ')
+  print('\n')
 
-#intro_screen()
+
+
+def determine_player_turn():
+    player_row = -1
+    while player_row not in [1,2,3]:
+      player_row = input('From which row do you want to remove stars on this turn? (1, 2, or 3): ')
+      if player_row.isdigit():
+        player_row = int(player_row)
+        if player_row in [1,2,3]:
+          if row_size[player_row] == 0:
+            print("There are no stars left in this row, choose again.")
+            player_row = -1
+        
+    player_num = -1
+    while player_num not in range(1,row_size[player_row]+1):
+      question = 'How many stars do you want to remove from Row ' + str(player_row) + '? '
+      player_num = input(question)
+      if player_num.isdigit():
+        player_num = int(player_num)
+    return player_row,player_num
+
+intro_screen()
 row_size = choose_row_sizes()
 print_screen(row_size)
 choose_turn = False
@@ -43,4 +66,18 @@ while choose_turn == False:
   elif turn == 'N' or turn == 'n':
       player_turn = 0
       choose_turn = True
-      
+is_winner = False
+while not is_winner:
+  if player_turn == 1:
+    player_row,player_num = determine_player_turn()
+    row_size[player_row] -= player_num
+    print_screen(row_size)
+    time.sleep(5)
+    player_turn = 0
+  else:
+    #this is where the computer calcs its move 
+    print('comp')
+    break
+    
+    
+    
